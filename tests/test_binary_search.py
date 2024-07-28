e65052f0e1a6e03a663736df2397a614d4e331ca
Hao Nguyen <hao@haonguyen.tech>
2024-07-29 01:16:25 +0700

    test: add binary_search

---
+++ tests/test_binary_search.py
@@ -0,0 +1,22 @@
+import pytest
+from binary_search import binary_search
+
+
+@pytest.mark.parametrize(
+    "sorted_list, target, expected_output",
+    [
+        ([1, 2, 3, 4, 5], 3, 2),
+        ([1, 2, 3, 4, 5], 6, None),
+        ([10, 20, 30, 40, 50], 10, 0),
+        ([10, 20, 30, 40, 50], 50, 4),
+        ([1, 3, 5, 7, 9], 2, None),
+        ([], 1, None),
+        ([1], 1, 0),
+        ([1], 0, None),
+        ([2, 4, 6, 8, 10], 4, 1),
+        ([2, 4, 6, 8, 10], 5, None),
+    ],
+)
+def test_binary_search(sorted_list, target, expected_output):
+    output = binary_search(sorted_list, target)
+    assert output == expected_output
