f8c1e0952745d1172783f3e45a421133be0de122
Hao Nguyen <hao@haonguyen.tech>
2024-08-04 02:17:48 +0700

    feat: add quick sort

---
+++ tests/test_quick_sort.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.sort.main import quick_sort
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ([4, 2, 6], [2, 4, 6]),
+        ([1, 0, 3, 8, 5, 16, 7], [0, 1, 3, 5, 7, 8, 16]),
+    ],
+)
+def test_quick_sort(input: list[int], expected: int) -> None:
+    output = quick_sort(input)
+    assert output == expected
