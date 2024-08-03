2a34a1628d1a138c372ba0b3cc7d5e692fdf6152
Hao Nguyen <hao@haonguyen.tech>
2024-08-03 18:42:38 +0700

    feat: add recursive_sum

--- src/recursive_sum.py
+++ src/recursive_sum.py
@@ -3,4 +3,4 @@ def recursive_sum(input_list: list[int]) -> int:
         return 0
     elif len(input_list) == 1:
         return input_list[0]
-    return recursive_sum([input_list[0]]) + recursive_sum(input_list[0:])
+    return recursive_sum([input_list[0]]) + recursive_sum(input_list[1:])
