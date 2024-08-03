2ef2960a79670c2b2d8039836dc3c3afb936da4a
Hao Nguyen <hao@haonguyen.tech>
2024-08-03 18:42:48 +0700

    test: for recursive sum

--- tests/test_recursive_sum.py
+++ tests/test_recursive_sum.py
@@ -1,9 +1,16 @@
-from src.sort.recoursive_sum import recursive_sum
+import pytest
+
+from src.recursive_sum import recursive_sum
 
 
-def test_recursive_sum():
-    input = [2, 4, 6]
-    expected = 12
-
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ([2, 4, 6], 12),
+        ([1, 2, 3], 6),
+        ([], 0),
+    ],
+)
+def test_recursive_sum(input: list[int], expected: int) -> None:
     output = recursive_sum(input_list=input)
     assert output == expected
