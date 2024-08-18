2aae5e88e6ddafe3a20af6d526615c3e10b88a1f
Hao Nguyen <hao@haonguyen.tech>
2024-08-18 18:30:23 +0700

    test(dijkstra_algo): assert the output path

--- tests/test_dijkstra_algo.py
+++ tests/test_dijkstra_algo.py
@@ -1,6 +1,11 @@
+import pytest
+
 from src.dijkstra_algo import dijkstra_algo
 
 
-def test_dijkstra_algo():
+def test_dijkstra_algo(capsys: pytest.CaptureFixture[str]):
     lowest_cost = dijkstra_algo()
     assert lowest_cost == 6
+
+    captured = capsys.readouterr()
+    assert "start -> b -> a -> fin" in captured.out
